8e186c2e6261cec75838c402b836e69ed27ba51c
Xan Bellón <xan.bellon@gmail.com>
2023-09-02 15:27:31 +0200

    test: refactor BasicCharacterTest
    Removed all single tests instantiations to a beforeEach approach

--- tests/Domain/Character/BasicCharacter.test.ts
+++ tests/Domain/Character/BasicCharacter.test.ts
@@ -5,23 +5,25 @@ import { Experience } from '@/Domain/Character/Experience'
 import { ExperienceDto } from '@/Domain/Character/ExperienceDto'
 import { Health } from '@/Domain/Character/Health'
 import { HitPointsDto } from '@/Domain/Character/HitPointsDto'
-import { describe, test, expect } from 'vitest'
+import { beforeEach, describe, test, expect } from 'vitest'
 
 describe('Testing BasicCharacter', () => {
+  let sut: BasicCharacter
+
+  beforeEach(() => {
+    sut = BasicCharacter.fromValues(CharacterName.fromString('Darling'), Experience.fromXp(345), Health.fromMaxHp(25))
+  })
   test('It should return proper CharacterId', () => {
-    const sut = BasicCharacter.fromValues(CharacterName.fromString('Darling'), Experience.fromXp(345), Health.fromMaxHp(25))
     const characterId = sut.id()
     expect(characterId).toBeInstanceOf(CharacterId)
     const againCharacterId = sut.id()
     expect(characterId.equals(againCharacterId))
   })
   test('It should return proper Name', () => {
-    const sut = BasicCharacter.fromValues(CharacterName.fromString('Darling'), Experience.fromXp(345), Health.fromMaxHp(25))
     expect(sut.name()).toBeInstanceOf(CharacterName)
     expect(sut.name().value()).equals('Darling')
   })
   test('It should return proper experience data', () => {
-    const sut = BasicCharacter.fromValues(CharacterName.fromString('Darling'), Experience.fromXp(345), Health.fromMaxHp(25))
     const values = sut.experience()
     expect(values).toBeInstanceOf(ExperienceDto)
     expect(values.actual).equals(345)
@@ -29,7 +31,6 @@ describe('Testing BasicCharacter', () => {
     expect(values.level).equals(2)
   })
   test('It should return proper hitpoints', () => {
-    const sut = BasicCharacter.fromValues(CharacterName.fromString('Darling'), Experience.fromXp(345), Health.fromMaxHp(25))
     const hitpoints = sut.hitPoints()
     expect(hitpoints).toBeInstanceOf(HitPointsDto)
     expect(hitpoints.max).equals(25)
