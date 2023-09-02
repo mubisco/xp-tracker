fafbbe4afbb01e3be8cbe376f12df0258cbe4b4e
Xan Bellón <xan.bellon@gmail.com>
2023-09-02 17:13:06 +0200

    test: refactor to remove repetitive instantiation

--- tests/Infrastructure/Character/Request/FormDataCharacterFactory.test.ts
+++ tests/Infrastructure/Character/Request/FormDataCharacterFactory.test.ts
@@ -1,49 +1,43 @@
 import { BasicCharacter } from '@/Domain/Character/BasicCharacter'
 import { CharacterFactoryError } from '@/Domain/Character/CharacterFactoryError'
 import { FormDataCharacterFactory } from '@/Infrastructure/Character/Request/FormDataCharacterFactory'
-import { describe, test, expect } from 'vitest'
+import { beforeEach, describe, test, expect } from 'vitest'
 
 describe('Testing BasicCharacter', () => {
-  test('It should be of proper class', () => {
-    const sut = new FormDataCharacterFactory()
-    expect(sut).toBeInstanceOf(FormDataCharacterFactory)
+  let sut: FormDataCharacterFactory
+  beforeEach(() => {
+    sut = new FormDataCharacterFactory()
   })
   test('It should throw error when data empty', () => {
-    const sut = new FormDataCharacterFactory()
     expect(() => {
       sut.make({})
     }).toThrow(CharacterFactoryError)
   })
   test('It should throw error when name data does not exists', () => {
-    const sut = new FormDataCharacterFactory()
     const data = { xp: 35000, hp: 54 }
     expect(() => {
       sut.make(data)
     }).toThrow(CharacterFactoryError)
   })
   test('It should throw error when xp data does not exists', () => {
-    const sut = new FormDataCharacterFactory()
     const data = { name: 'Darling', hp: 54 }
     expect(() => {
       sut.make(data)
     }).toThrow(CharacterFactoryError)
   })
   test('It should throw error when hp data does not exists', () => {
-    const sut = new FormDataCharacterFactory()
     const data = { name: 'Darling', xp: 5400 }
     expect(() => {
       sut.make(data)
     }).toThrow(CharacterFactoryError)
   })
   test('It should throw error when xp wrong value', () => {
-    const sut = new FormDataCharacterFactory()
     const data = { name: 'Darling', hp: 54, xp: -1 }
     expect(() => {
       sut.make(data)
     }).toThrow(CharacterFactoryError)
   })
   test('It should return proper Character instance', () => {
-    const sut = new FormDataCharacterFactory()
     const character = sut.make({ name: 'Darling', xp: 35000, hp: 54 })
     expect(character).toBeInstanceOf(BasicCharacter)
     const basicCharacter = character as BasicCharacter
