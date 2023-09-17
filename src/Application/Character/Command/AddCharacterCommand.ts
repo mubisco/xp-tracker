2e92261043213fc2582da7c653a43643146f1fc6
Xan Bellón <xan.bellon@gmail.com>
2023-09-17 16:06:22 +0200

    chore: refactor AddCharacterCommand

--- src/Application/Character/Command/AddCharacterCommand.ts
+++ src/Application/Character/Command/AddCharacterCommand.ts
@@ -1,11 +1,9 @@
 export class AddCharacterCommand {
-  public readonly name: string
-  public readonly actualXp: number
-  public readonly maxHp: number
-
-  constructor (name: string, actualXp: number, maxHp: number) {
-    this.name = name
-    this.actualXp = actualXp
-    this.maxHp = maxHp
+  /* eslint-disable no-useless-constructor */
+  constructor (
+    readonly name: string,
+    readonly actualXp: number,
+    readonly maxHp: number
+  ) {
   }
 }
