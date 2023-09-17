3671fe683f06f223b7c24d769c7df4878395887f
Xan Bellón <xan.bellon@gmail.com>
2023-09-17 16:11:22 +0200

    refactor: simplify constructor

--- src/Domain/Character/Health.ts
+++ src/Domain/Character/Health.ts
@@ -1,9 +1,6 @@
 import { HitPointsDto } from './HitPointsDto'
 
 export class Health {
-  private _maxHp: number
-  private _currentHp: number
-
   static fromMaxHp (maxHp: number) {
     return new this(maxHp, maxHp)
   }
@@ -12,15 +9,13 @@ export class Health {
     return new this(maxHp, currentHp)
   }
 
-  private constructor (maxHp: number, currentHp: number) {
-    if (maxHp < 0) {
+  private constructor (private readonly maxHp: number, private readonly currentHp: number) {
+    if (this.maxHp < 0) {
       throw new RangeError('Max hitpoints should be 0 or greater!')
     }
-    this._maxHp = maxHp
-    this._currentHp = currentHp
   }
 
   hitpoints (): HitPointsDto {
-    return new HitPointsDto(this._maxHp, this._currentHp)
+    return new HitPointsDto(this.maxHp, this.currentHp)
   }
 }
