5452cd8278d332b58c1e32f27f5cb995481335c0
Xan Bellón <xan.bellon@gmail.com>
2023-09-17 16:08:06 +0200

    refactor: simplify constructor

--- src/Domain/Character/CharacterName.ts
+++ src/Domain/Character/CharacterName.ts
@@ -1,15 +1,12 @@
 export class CharacterName {
-  private _value: string
-
   static fromString (name: string): CharacterName {
     return new this(name)
   }
 
-  private constructor (name: string) {
-    if (name.length === 0) {
+  private constructor (private readonly _value: string) {
+    if (this._value.length === 0) {
       throw new RangeError('Name must be not empty')
     }
-    this._value = name
   }
 
   value (): string {
