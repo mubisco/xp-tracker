d419b3a8f38f376ad42ae56560bcbe31a3d94a01
Xan Bellón <xan.bellon@gmail.com>
2023-10-01 13:42:23 +0200

    refactor: splitted bug component into pieces
    Now we have the load encounter use case integrated into the UI,
    we thought that the original component was too overloaded, so we
    have splitted it into three sub-components.

--- src/UI/components/Encounters/EditEncounterForm.vue
+++ src/UI/components/Encounters/EditEncounterForm.vue
@@ -4,6 +4,8 @@ import { EncounterDto } from '@/Domain/Encounter/EncounterDto'
 import { FindEncounterByIdQueryHandlerProvider } from '@/Infrastructure/Encounter/Provider/FindEncounterByIdQueryHandlerProvider'
 import { computed, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import EncounterDetails from './EncounterDetails.vue'
+import AddEncounterDetailForm from './AddEncounterDetailForm.vue'
 
 const provider = new FindEncounterByIdQueryHandlerProvider()
 const encounter = ref<EncounterDto | null>(null)
@@ -19,133 +21,16 @@ onMounted(async () => {
   encounter.value = await useCase.handle(query)
 })
 
-const currentMonsters = ref([
-  {
-    name: 'Shadow Dancer',
-    cr: 7,
-    xp: 2900
-  },
-  {
-    name: 'Shadow Dancer',
-    cr: 7,
-    xp: 2900
-  }
-])
-
-const addRow = () => {
-  console.log('Row should be added')
-}
 </script>
 
 <template>
   <v-card>
     <template #title>
-      Edit {{ encounterName }} Ecounter
+      Edit {{ encounterName }} Encounter
     </template>
     <template #text>
-      <v-table density="compact">
-        <thead>
-          <tr>
-            <th class="text-left">
-              Name
-            </th>
-            <th class="text-right">
-              CR
-            </th>
-            <th class="text-right">
-              XP
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="(monster, index) in currentMonsters"
-            :key="index"
-          >
-            <td>{{ monster.name }}</td>
-            <td class="text-right">
-              {{ monster.cr }}
-            </td>
-            <td class="text-right">
-              {{ monster.xp }}
-            </td>
-          </tr>
-        </tbody>
-        <tfoot>
-          <tr>
-            <th
-              class="text-right"
-              colspan="2"
-            >
-              Total Monsters
-            </th>
-            <td
-              class="text-right"
-            >
-              2
-            </td>
-          </tr>
-          <tr>
-            <th
-              class="text-right"
-              colspan="2"
-            >
-              Total XP
-            </th>
-            <td
-              class="text-right"
-            >
-              5800
-            </td>
-          </tr>
-          <tr>
-            <th
-              class="text-right"
-              colspan="2"
-            >
-              Total Points
-            </th>
-            <td
-              class="text-right"
-            >
-              7200
-            </td>
-          </tr>
-        </tfoot>
-      </v-table>
-      <v-form>
-        <v-row>
-          <v-col
-            cols="6"
-          >
-            <v-text-field
-              label="Name"
-              density="compact"
-              type="text"
-            />
-          </v-col>
-          <v-col
-            cols="3"
-          >
-            <v-text-field
-              label="XP"
-              density="compact"
-              type="number"
-            />
-          </v-col>
-          <v-col
-            cols="3"
-          >
-            <v-text-field
-              label="CR"
-              density="compact"
-              type="number"
-              append-icon="mdi-send"
-              @click:append="addRow"
-            />
-          </v-col>
-        </v-row>
-      </v-form>
+      <EncounterDetails />
+      <AddEncounterDetailForm />
     </template>
     <v-card-actions class="justify-space-between">
       <v-btn
---
+++ src/UI/components/Encounters/EncounterDetails.vue
@@ -0,0 +1,90 @@
+<script lang="ts" setup>
+
+import { ref } from 'vue'
+const currentMonsters = ref([
+  {
+    name: 'Shadow Dancer',
+    cr: 7,
+    xp: 2900
+  },
+  {
+    name: 'Shadow Dancer',
+    cr: 7,
+    xp: 2900
+  }
+])
+
+</script>
+
+<template>
+  <v-table density="compact">
+    <thead>
+      <tr>
+        <th class="text-left">
+          Name
+        </th>
+        <th class="text-right">
+          CR
+        </th>
+        <th class="text-right">
+          XP
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="(monster, index) in currentMonsters"
+        :key="index"
+      >
+        <td>{{ monster.name }}</td>
+        <td class="text-right">
+          {{ monster.cr }}
+        </td>
+        <td class="text-right">
+          {{ monster.xp }}
+        </td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <tr>
+        <th
+          class="text-right"
+          colspan="2"
+        >
+          Total Monsters
+        </th>
+        <td
+          class="text-right"
+        >
+          2
+        </td>
+      </tr>
+      <tr>
+        <th
+          class="text-right"
+          colspan="2"
+        >
+          Total XP
+        </th>
+        <td
+          class="text-right"
+        >
+          5800
+        </td>
+      </tr>
+      <tr>
+        <th
+          class="text-right"
+          colspan="2"
+        >
+          Total Points
+        </th>
+        <td
+          class="text-right"
+        >
+          7200
+        </td>
+      </tr>
+    </tfoot>
+  </v-table>
+</template>
