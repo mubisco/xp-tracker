bb9b290e0aee25798ac4461bacf6ca6502e9244e
Xan Bellón <xan.bellon@gmail.com>
2023-10-07 20:34:06 +0200

    feat: add a way to add monsters to an encounter
    We know we should add monster to an encounter. First naive
    implementation to have this feature working, maybe later we
    should remove the monsters accessor for better encapsulation

--- tests/Domain/Encounter/DomainEncounter.test.ts
+++ tests/Domain/Encounter/DomainEncounter.test.ts
@@ -3,6 +3,7 @@ import { Ulid } from '@/Domain/Shared/Identity/Ulid'
 import { describe, expect, test } from 'vitest'
 import { DomainEncounterOM } from './DomainEncounterOM'
 import { SimpleStringEncounterVisitor } from './SimpleStringEncounterVisitor'
+import { EncounterMonster } from '@/Domain/Encounter/Monster/EncounterMonster'
 
 describe('Testing DomainEncounter', () => {
   const visitor = new SimpleStringEncounterVisitor()
@@ -12,7 +13,18 @@ describe('Testing DomainEncounter', () => {
     expect(sut.id()).toBeInstanceOf(Ulid)
     expect(sut.name()).toBeInstanceOf(EncounterName)
     expect(sut.name().value()).toBe('pollos')
+    expect(sut.monsters()).toHaveLength(0)
   })
+
+  test('It should add a monster properly', () => {
+    const monsterToAdd = EncounterMonster.fromValues('Pollo Papi', 2500, '1/2')
+    const sut = DomainEncounterOM.withName('pollos')
+    sut.addMonster(monsterToAdd)
+    const monsters = sut.monsters()
+    expect(monsters).toHaveLength(1)
+    expect(monsters[0]).toStrictEqual(monsterToAdd)
+  })
+
   test('it should return proper visitor result', () => {
     const sut = DomainEncounterOM.withName('pollos')
     const result = sut.visit(visitor)
