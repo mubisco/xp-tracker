45b558b595b9b24a3a04c8e089fa53fc40d548df
Xan Bellón <xan.bellon@gmail.com>
2023-10-07 21:58:30 +0200

    feat: add method to addMonster
    Should be commited int the last commit.

--- src/Domain/Encounter/Encounter.ts
+++ src/Domain/Encounter/Encounter.ts
@@ -1,7 +1,9 @@
 import { Ulid } from '@/Domain/Shared/Identity/Ulid'
 import { EncounterVisitor } from './EncounterVisitor'
+import { EncounterMonster } from './Monster/EncounterMonster'
 
 export interface Encounter {
   id(): Ulid
+  addMonster (monster: EncounterMonster): void
   visit (visitor: EncounterVisitor<any>): any
 }
