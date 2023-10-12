910be07a1472e3108c01a6bd63a8132f1bc26cd6
Xan Bellón <xan.bellon@gmail.com>
2023-10-12 18:51:42 +0200

    feat: add monster parsing on EncounterVisitor

--- tests/Infrastructure/Encounter/Persistence/LocalStorage/LocalStorageEncounterSerializerVisitor.test.ts
+++ tests/Infrastructure/Encounter/Persistence/LocalStorage/LocalStorageEncounterSerializerVisitor.test.ts
@@ -1,5 +1,6 @@
 import { LocalStorageEncounterSerializerVisitor } from '@/Infrastructure/Encounter/Persistence/Storage/LocalStorageEncounterSerializerVisitor'
 import { DomainEncounterOM } from '@tests/Domain/Encounter/DomainEncounterOM'
+import { EncounterMonsterOM } from '@tests/Domain/Encounter/EncounterMonsterOM'
 import { describe, expect, test } from 'vitest'
 
 describe('Testing LocalStorageEncounterSerializerVisitor', () => {
@@ -7,7 +8,26 @@ describe('Testing LocalStorageEncounterSerializerVisitor', () => {
     const sut = new LocalStorageEncounterSerializerVisitor()
     const encounter = DomainEncounterOM.withName('Pollos')
     const result = encounter.visit(sut)
-    const expectedResult = `{"ulid":"${encounter.id().value()}","name":"Pollos"}`
+    const expectedResult = `{"ulid":"${encounter.id().value()}","name":"Pollos","monsters":[]}`
     expect(result).toBe(expectedResult)
+  })
+  test('It should visit encounter with monsters properly', () => {
+    const sut = new LocalStorageEncounterSerializerVisitor()
+    const encounter = DomainEncounterOM.withName('Pollos')
+    const monster = EncounterMonsterOM.random()
+    encounter.addMonster(monster)
+    const result = encounter.visit(sut)
+    const expectedResult = {
+      ulid: encounter.id().value(),
+      name: 'Pollos',
+      monsters: [
+        {
+          name: 'Some name',
+          xp: 2500,
+          cr: '1/2'
+        }
+      ]
+    }
+    expect(result).toBe(JSON.stringify(expectedResult))
   })
 })
