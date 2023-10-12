dc98d35d2f2e62de691ca1b7df50cf8f9328c8e1
Xan Bellón <xan.bellon@gmail.com>
2023-10-12 19:04:23 +0200

    refactor: reviewed providers
    As result of our new dependency on LocalStorageEncounterRepository, we've
    added on all Providers the instantiation of these new dependencies

--- src/Infrastructure/Encounter/Provider/AddEncounterCommandHandlerProvider.ts
+++ src/Infrastructure/Encounter/Provider/AddEncounterCommandHandlerProvider.ts
@@ -1,11 +1,13 @@
 import { AddEncounterCommandHandler } from '@/Application/Encounter/Command/AddEncounterCommandHandler'
 import { LocalStorageEncounterRepository } from '@/Infrastructure/Encounter/Persistence/Storage/LocalStorageEncounterRepository'
 import { LocalStorageEncounterSerializerVisitor } from '@/Infrastructure/Encounter/Persistence/Storage/LocalStorageEncounterSerializerVisitor'
+import { LocalStorageEncounterFactory } from '@/Infrastructure/Encounter/Persistence/Storage/LocalStorageEncounterFactory'
 
 export class AddEncounterCommandHandlerProvider {
   provide (): AddEncounterCommandHandler {
     const writeModel = new LocalStorageEncounterRepository(
-      new LocalStorageEncounterSerializerVisitor()
+      new LocalStorageEncounterSerializerVisitor(),
+      new LocalStorageEncounterFactory()
     )
     return new AddEncounterCommandHandler(writeModel)
   }
