d481b8422a1686415d0e5cd6a313849050225200
Xan Bellón <xan.bellon@gmail.com>
2023-10-12 19:00:27 +0200

    feat: add monster hydration in Factory
    As we decided to make hydrations avoiding constructors to have semantic
    constructors, we make some cheating here, assigning properties of the object
    directly. That's why we need to disable ts warnings in these two lines.

--- src/Infrastructure/Encounter/Persistence/Storage/LocalStorageEncounterFactory.ts
+++ src/Infrastructure/Encounter/Persistence/Storage/LocalStorageEncounterFactory.ts
@@ -1,12 +1,23 @@
 import { DomainEncounter } from '@/Domain/Encounter/DomainEncounter'
 import { EncounterDto } from '@/Domain/Encounter/EncounterDto'
 import { EncounterName } from '@/Domain/Encounter/EncounterName'
+import { EncounterMonster } from '@/Domain/Encounter/Monster/EncounterMonster'
+import { MonsterDto } from '@/Domain/Encounter/MonsterDto'
 import { Ulid } from '@/Domain/Shared/Identity/Ulid'
 
 export class LocalStorageEncounterFactory {
   make (encounterData: EncounterDto): DomainEncounter {
     const encounter = DomainEncounter.withName(EncounterName.fromString(encounterData.name))
+    // @ts-ignore
     encounter.ulid = Ulid.fromString(encounterData.ulid)
+    // @ts-ignore
+    encounter._encounterMonsters = this.hydrateMonsters(encounterData.monsters)
     return encounter
+  }
+
+  private hydrateMonsters (monsters: MonsterDto[]): EncounterMonster[] {
+    return monsters.map((monster: MonsterDto): EncounterMonster => {
+      return EncounterMonster.fromValues(monster.name, monster.xp, monster.cr)
+    })
   }
 }
--- tests/Infrastructure/Encounter/Persistence/LocalStorage/LocalStorageEncounterFactory.test.ts
+++ tests/Infrastructure/Encounter/Persistence/LocalStorage/LocalStorageEncounterFactory.test.ts
@@ -12,4 +12,18 @@ describe('Testing LocalStorageEncounterFactory', () => {
     expect(result.name().value()).toBe(dto.name)
     expect(result.monsters()).toHaveLength(0)
   })
+
+  test('It should return proper DomainEncounter when monsters added', () => {
+    const sut = new LocalStorageEncounterFactory()
+    const dto = {
+      ulid: '01HCJBW16FTABFGZRC5V3835AP',
+      name: 'Encounter name',
+      monsters: [
+        { name: 'Orco feo', xp: 500, cr: '1/2' },
+        { name: 'Kobold chungo', xp: 400, cr: '1/4' }
+      ]
+    }
+    const result = sut.make(dto)
+    expect(result.monsters()).toHaveLength(2)
+  })
 })
