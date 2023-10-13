44951e0a9b017289558ef33e73bc833821df8088
Xan Bellón <xan.bellon@gmail.com>
2023-10-13 12:20:06 +0200

    test: refactored test

--- tests/Application/Encounter/Command/DeleteMonsterFromEncounterCommandHandler.test.ts
+++ tests/Application/Encounter/Command/DeleteMonsterFromEncounterCommandHandler.test.ts
@@ -1,19 +1,23 @@
 import { DeleteMonsterFromEncounterCommand } from '@/Application/Encounter/Command/DeleteMonsterFromEncounterCommand'
 import { DeleteMonsterFromEncounterCommandHandler } from '@/Application/Encounter/Command/DeleteMonsterFromEncounterCommandHandler'
 import { EncounterNotFoundError } from '@/Domain/Encounter/EncounterNotFoundError'
-import { describe, expect, test } from 'vitest'
+import { beforeEach, describe, expect, test } from 'vitest'
 
 describe('Testing DeleteMonsterFromEncounterCommandHandler', () => {
+  let sut: DeleteMonsterFromEncounterCommandHandler
+  const command = new DeleteMonsterFromEncounterCommand('01HCM7C58CY0CAW9F8WV5QCM0T')
+
+  beforeEach(() => {
+    sut = new DeleteMonsterFromEncounterCommandHandler()
+  })
   test('It should be of proper class', () => {
-    const sut = new DeleteMonsterFromEncounterCommandHandler()
     expect(sut).toBeInstanceOf(DeleteMonsterFromEncounterCommandHandler)
   })
   test('It should throw error when encounter id is wrong', () => {
-    const sut = new DeleteMonsterFromEncounterCommandHandler()
-    expect(sut.handle(new DeleteMonsterFromEncounterCommand('01HCM7C58CY0CAW9F8WV5QCM0'))).rejects.toThrow(RangeError)
+    const wrongEncounterUlidCommand = new DeleteMonsterFromEncounterCommand('01HCM7C58CY0CAW9F8WV5QCM0')
+    expect(sut.handle(wrongEncounterUlidCommand)).rejects.toThrow(RangeError)
   })
   test('It should throw error when encounter not found', () => {
-    const sut = new DeleteMonsterFromEncounterCommandHandler()
-    expect(sut.handle(new DeleteMonsterFromEncounterCommand('01HCM7C58CY0CAW9F8WV5QCM0T'))).rejects.toThrow(EncounterNotFoundError)
+    expect(sut.handle(command)).rejects.toThrow(EncounterNotFoundError)
   })
 })
