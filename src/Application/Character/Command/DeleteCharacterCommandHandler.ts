6b2052d03a0545d273ae6f733be8b7459899fe46
Xan Bellón <xan.bellon@gmail.com>
2023-10-29 22:36:34 +0100

    feat: add event when CharacterDeleted

--- src/Application/Character/Command/DeleteCharacterCommandHandler.ts
+++ src/Application/Character/Command/DeleteCharacterCommandHandler.ts
@@ -1,14 +1,20 @@
 import { Ulid } from '@/Domain/Shared/Identity/Ulid'
 import { DeleteCharacterCommand } from './DeleteCharacterCommand'
 import { DeleteCharacterWriteModel } from '@/Domain/Character/DeleteCharacterWriteModel'
+import { EventBus } from '@/Domain/Shared/Event/EventBus'
+import { PartyWasUpdated } from '@/Domain/Character/Party/PartyWasUpdated'
 
 export class DeleteCharacterCommandHandler {
   /* eslint-disable no-useless-constructor */
-  constructor (private readonly writeModel: DeleteCharacterWriteModel) {
+  constructor (
+    private readonly writeModel: DeleteCharacterWriteModel,
+    private readonly eventBus: EventBus
+  ) {
   }
 
   async handle (command: DeleteCharacterCommand): Promise<void> {
     const characterUlid = Ulid.fromString(command.characterId)
+    this.eventBus.publish([new PartyWasUpdated()])
     return this.writeModel.remove(characterUlid)
   }
 }
