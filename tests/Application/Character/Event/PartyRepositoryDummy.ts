807d48ce931192fead41e481068c3a2a1571a454
Xan Bellón <xan.bellon@gmail.com>
2023-12-06 13:43:23 +0100

    feat: manage event when update party experience

--- tests/Application/Character/Event/PartyRepositoryDummy.ts
+++ tests/Application/Character/Event/PartyRepositoryDummy.ts
@@ -1,12 +1,20 @@
 import { Experience } from '@/Domain/Character/Experience'
 import { Party } from '@/Domain/Character/Party/Party'
 import { PartyRepository } from '@/Domain/Character/Party/PartyRepository'
+import { PartyWasUpdated } from '@/Domain/Character/Party/PartyWasUpdated'
+import { DomainEvent } from '@/Domain/Shared/Event/DomainEvent'
 
 class PartyDummySpy implements Party {
   public experiencePoints: number = 0
+  private _events: DomainEvent[] = []
   // eslint-disable-next-line
   updateExperience (experiencePoints: Experience): void {
     this.experiencePoints = experiencePoints.values().actual
+    this._events.push(new PartyWasUpdated())
+  }
+
+  pullEvents (): DomainEvent[] {
+    return this._events
   }
 }
 
--- tests/Application/Character/Event/UpdatePartyExperienceWhenEncounterWasFinishedEventHandler.test.ts
+++ tests/Application/Character/Event/UpdatePartyExperienceWhenEncounterWasFinishedEventHandler.test.ts
@@ -8,6 +8,8 @@ import { FailingPartyRepositoryDummy } from './FailingPartyRepositoryDummy'
 import { PartyWriteModelError } from '@/Domain/Character/Party/PartyWriteModelError'
 import { UpdateExperiencePartyWriteModelSpy } from './UpdateExperiencePartyWriteModelSpy'
 import { FailingUpdateExperiencePartyWriteModel } from './FailingUpdateExperiencePartyWriteModel'
+import { InMemoryEventBusSpy } from '@tests/Application/Encounter/Command/InMemoryEventBusSpy'
+import { PartyWasUpdated } from '@/Domain/Character/Party/PartyWasUpdated'
 
 describe('Testing UpdatePartyExperienceWhenEncounterWasFinishedEventHandler', () => {
   let failingPartyRepository: FailingPartyRepositoryDummy
@@ -15,6 +17,7 @@ describe('Testing UpdatePartyExperienceWhenEncounterWasFinishedEventHandler', ()
   let partyRepositoryDummy: PartyRepositoryDummy
   let failingWriteModel: FailingUpdateExperiencePartyWriteModel
   let writeModelSpy: UpdateExperiencePartyWriteModelSpy
+  let eventBusSpy: InMemoryEventBusSpy
 
   beforeEach(() => {
     failingPartyRepository = new FailingPartyRepositoryDummy()
@@ -22,35 +25,43 @@ describe('Testing UpdatePartyExperienceWhenEncounterWasFinishedEventHandler', ()
     partyRepositoryDummy = new PartyRepositoryDummy()
     failingWriteModel = new FailingUpdateExperiencePartyWriteModel()
     writeModelSpy = new UpdateExperiencePartyWriteModelSpy()
+    eventBusSpy = new InMemoryEventBusSpy()
   })
   test('It should throw error when Experience points not valid', () => {
-    const sut = new UpdatePartyExperienceWhenEncounterWasFinishedEventHandler(failingPartyRepository, failingWriteModel)
+    const sut = new UpdatePartyExperienceWhenEncounterWasFinishedEventHandler(failingPartyRepository, failingWriteModel, eventBusSpy)
     const badEvent = new EncounterWasFinished('01HCSAPRM9H79111WHMYRGYSB3', -1)
     expect(sut.handle(badEvent)).rejects.toThrow(RangeError)
   })
   test('It should throw error when party cannot be loaded', () => {
-    const sut = new UpdatePartyExperienceWhenEncounterWasFinishedEventHandler(failingPartyRepository, failingWriteModel)
+    const sut = new UpdatePartyExperienceWhenEncounterWasFinishedEventHandler(failingPartyRepository, failingWriteModel, eventBusSpy)
     const event = new EncounterWasFinished('01HCSAPRM9H79111WHMYRGYSB3', 250)
     expect(sut.handle(event)).rejects.toThrow(PartyRepositoryError)
   })
   test('It should throw error when party experience cannot be updated', () => {
-    const sut = new UpdatePartyExperienceWhenEncounterWasFinishedEventHandler(notUpdatablePartyRepository, failingWriteModel)
+    const sut = new UpdatePartyExperienceWhenEncounterWasFinishedEventHandler(notUpdatablePartyRepository, failingWriteModel, eventBusSpy)
     const event = new EncounterWasFinished('01HCSAPRM9H79111WHMYRGYSB3', 250)
     expect(sut.handle(event)).rejects.toThrow(RangeError)
   })
   test('It should throw error when party cannot be updated', () => {
-    const sut = new UpdatePartyExperienceWhenEncounterWasFinishedEventHandler(partyRepositoryDummy, failingWriteModel)
+    const sut = new UpdatePartyExperienceWhenEncounterWasFinishedEventHandler(partyRepositoryDummy, failingWriteModel, eventBusSpy)
     const event = new EncounterWasFinished('01HCSAPRM9H79111WHMYRGYSB3', 250)
     expect(sut.handle(event)).rejects.toThrow(PartyWriteModelError)
   })
   test('It should update experience points properly', async () => {
-    const sut = new UpdatePartyExperienceWhenEncounterWasFinishedEventHandler(partyRepositoryDummy, writeModelSpy)
+    const sut = new UpdatePartyExperienceWhenEncounterWasFinishedEventHandler(partyRepositoryDummy, writeModelSpy, eventBusSpy)
     const event = new EncounterWasFinished('01HCSAPRM9H79111WHMYRGYSB3', 250)
     await sut.handle(event)
     expect(writeModelSpy.updatedPoints).toBe(250)
   })
+  test('It should push events when experience updated', async () => {
+    const sut = new UpdatePartyExperienceWhenEncounterWasFinishedEventHandler(partyRepositoryDummy, writeModelSpy, eventBusSpy)
+    const event = new EncounterWasFinished('01HCSAPRM9H79111WHMYRGYSB3', 250)
+    await sut.handle(event)
+    expect(eventBusSpy.events).toHaveLength(1)
+    expect(eventBusSpy.events[0]).toBeInstanceOf(PartyWasUpdated)
+  })
   test('It should return true to appropiate event', () => {
-    const sut = new UpdatePartyExperienceWhenEncounterWasFinishedEventHandler(partyRepositoryDummy, writeModelSpy)
+    const sut = new UpdatePartyExperienceWhenEncounterWasFinishedEventHandler(partyRepositoryDummy, writeModelSpy, eventBusSpy)
     expect(sut.listensTo('EncounterWasFinished')).toBe(true)
   })
 })
