133eded078463dc62d6322f82c91c5f927c96853
Xan Bellón <xan.bellon@gmail.com>
2023-12-06 12:26:11 +0100

    feat: configure eventHandler to be on EventBus

--- src/UI/plugins/eventSubscribe.ts
+++ src/UI/plugins/eventSubscribe.ts
@@ -2,11 +2,14 @@ import { App } from 'vue'
 import { EventBus } from '@/Domain/Shared/Event/EventBus'
 import { VueEventBus } from '@/Infrastructure/Shared/Event/VueEventBus'
 import { UpdatePartyExperienceWhenEncounterWasFinishedProvider } from '@/Infrastructure/Character/Provider/UpdatePartyExperienceWhenEncounterWasFinishedProvider'
+import { UpdateEncounterLevelsWhenPartyUpdatedProvider } from '@/Infrastructure/Encounter/Provider/UpdateEncounterLevelsWhenPartyUpdatedProvider'
 
 export function eventSubscribe (app: App): void {
   const vueEventBus = new VueEventBus()
   const provider = new UpdatePartyExperienceWhenEncounterWasFinishedProvider()
+  const updateEncounterLevelWhenPartyWasUpdatedProvider = new UpdateEncounterLevelsWhenPartyUpdatedProvider()
 
   vueEventBus.subscribe(provider.provide())
+  vueEventBus.subscribe(updateEncounterLevelWhenPartyWasUpdatedProvider.provide())
   app.provide<EventBus>('eventBus', vueEventBus)
 }
