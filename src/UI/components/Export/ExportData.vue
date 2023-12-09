7bf4ffbc2ca35dccff85b3d7bf594bcdff781957
Xan Bellón <xan.bellon@gmail.com>
2023-12-09 12:48:09 +0100

    feat: integrate export config usecase into UI

--- src/UI/components/Export/ExportData.vue
+++ src/UI/components/Export/ExportData.vue
@@ -1,4 +1,32 @@
 <script lang="ts" setup>
+import { ExportDataDto } from '@/Application/Configuration/Query/ExportDataDto'
+import { ExportDataQuery } from '@/Application/Configuration/Query/ExportDataQuery'
+import { ExportDataQueryHandlerProvider } from '@/Infrastructure/Configuration/Provider/ExportDataQueryHandlerProvider'
+import { useSnackbarStore } from '@/UI/store/snackbar'
+import { ref } from 'vue'
+
+const filename = ref('')
+const provider = new ExportDataQueryHandlerProvider()
+const snackbarStore = useSnackbarStore()
+
+const downloadItem = async () => {
+  const data = await exportData()
+  const content = atob(data.base64content)
+  const blob = new Blob([content], { type: 'application/json' })
+  const link = document.createElement('a')
+  link.href = URL.createObjectURL(blob)
+  link.download = data.filename
+  link.click()
+  URL.revokeObjectURL(link.href)
+  snackbarStore.addMessage(`File ${filename.value} downloaded successfully!!!`, 'success')
+  filename.value = ''
+}
+
+const exportData = async (): Promise<ExportDataDto> => {
+  const query = new ExportDataQuery(filename.value)
+  const handler = provider.provide()
+  return await handler.handle(query)
+}
 </script>
 
 <template>
@@ -8,6 +36,7 @@
     </template>
     <template #text>
       <v-text-field
+        v-model="filename"
         label="File name"
         type="text"
       />
@@ -17,6 +46,7 @@
         variant="elevated"
         color="primary"
         prepend-icon="mdi-content-save"
+        @click.prevent="downloadItem()"
       >
         Save
       </v-btn>
