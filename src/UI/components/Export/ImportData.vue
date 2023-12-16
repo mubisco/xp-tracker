fc669a1b4183a27dbc191f8fd384dcc70579fa39
Xan Bellón <xan.bellon@gmail.com>
2023-12-16 18:51:26 +0100

    feat: implement import data use case on UX

--- src/UI/components/Export/ImportData.vue
+++ src/UI/components/Export/ImportData.vue
@@ -1,4 +1,40 @@
 <script lang="ts" setup>
+import { ImportDataCommand } from '@/Application/Configuration/Command/ImportDataCommand'
+import { ImportDataCommandHandlerProvider } from '@/Infrastructure/Configuration/Provider/ImportDataCommandHandlerProvider'
+import { useSnackbarStore } from '@/UI/store/snackbar'
+import { computed, ref } from 'vue'
+
+const snackbarStore = useSnackbarStore()
+const provider = new ImportDataCommandHandlerProvider()
+const content = ref('')
+
+const readyToUpload = computed((): boolean => {
+  return content.value !== ''
+})
+
+const readFiles = (event: Event) => {
+  content.value = ''
+  const eventTarget = event.target as HTMLInputElement | null
+  const files = eventTarget?.files
+  const file = files ? files[0] : null
+  if (file === null) {
+    return
+  }
+  const reader = new FileReader()
+  reader.readAsText(file, 'UTF-8');
+  reader.onloadend = (readerEvent: ProgressEvent<FileReader>) => {
+    if (readerEvent?.target?.result) {
+      content.value = readerEvent.target.result.toString()
+    }
+  }
+}
+
+const importData = async (): Promise<void> => {
+  const command = new ImportDataCommand(btoa(content.value))
+  const handler = provider.provide()
+  await handler.handle(command)
+  snackbarStore.addMessage('Data imported successfully!!!', 'success')
+}
 </script>
 
 <template>
@@ -10,6 +46,7 @@
       <v-file-input
         accept=".json"
         label="Select a json file to import data"
+        @change="readFiles"
       />
     </template>
     <v-card-actions class="justify-end">
@@ -17,6 +54,8 @@
         variant="elevated"
         color="primary"
         prepend-icon="mdi-upload"
+        :disabled="!readyToUpload"
+        @click="importData"
       >
         Upload
       </v-btn>
