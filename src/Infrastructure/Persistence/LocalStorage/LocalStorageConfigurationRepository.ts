0105d5c51854fbb7628113d96e8eeb723aade59c
Xan Bellón <xan.bellon@gmail.com>
2023-12-16 17:58:35 +0100

    feat: Implement writeConfiguration

--- src/Infrastructure/Persistence/LocalStorage/LocalStorageConfigurationRepository.ts
+++ src/Infrastructure/Persistence/LocalStorage/LocalStorageConfigurationRepository.ts
@@ -1,8 +1,18 @@
 import { ConfigurationReadModel } from '@/Domain/Configuration/ConfigurationReadModel'
 import { LocalStorageTag } from '../LocalStorageTag'
+import { ConfigurationWriteModel } from '@/Domain/Configuration/ConfigurationWriteModel'
+import { ConfigurationContent } from '@/Domain/Configuration/ConfigurationContent'
 
-export class LocalStorageConfigurationRepository implements ConfigurationReadModel {
-  toBase64 (): Promise<string> {
+export class LocalStorageConfigurationRepository implements ConfigurationReadModel, ConfigurationWriteModel {
+  async writeConfiguration (content: ConfigurationContent): Promise<void> {
+    const parsedData = JSON.parse(content.value())
+    const characters = parsedData.characters
+    const encounters = parsedData.encounters
+    localStorage.setItem(LocalStorageTag.CHARACTERS, JSON.stringify(characters))
+    localStorage.setItem(LocalStorageTag.ENCOUNTERS, JSON.stringify(encounters))
+  }
+
+  async toBase64 (): Promise<string> {
     const characters = localStorage.getItem(LocalStorageTag.CHARACTERS) ?? '{}'
     const encounters = localStorage.getItem(LocalStorageTag.ENCOUNTERS) ?? '{}'
     const data = { characters: JSON.parse(characters), encounters: JSON.parse(encounters) }
