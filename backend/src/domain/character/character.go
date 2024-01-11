d16fe442ba969795a3187399bb258a5bec06e9b7
Xan Bellón <xan.bellon@gmail.com>
2024-01-11 08:25:36 +0100

    feat: implement some kind of constructor

--- backend/src/domain/character/character.go
+++ backend/src/domain/character/character.go
@@ -1,7 +1,13 @@
 package character
 
 // func NewCharacter (ulid string, name string, actualXp int, maxHp int) (Character, error) {
-// }
+func NewCharacter (ulid string) (Character, error) {
+	characterId, error := NewCharacterId(ulid)
+  if error != nil {
+    return Character {FromEmpty()}, error
+  }
+  return Character{characterId}, nil
+}
 
 type Character struct {
   characterId CharacterId
--- backend/src/domain/character/character_test.go
+++ backend/src/domain/character/character_test.go
@@ -4,36 +4,31 @@ import (
 	"reflect"
 	"testing"
 )
-const ULID = "01AN4Z07BY79KA1307SR9X4MV3";
+
+const ULID = "01AN4Z07BY79KA1307SR9X4MV3"
 
 func TestInstanceOf(t *testing.T) {
-	sut := dummyCharacter()
+	sut, _ := dummyCharacter()
 	instance := reflect.TypeOf(sut).String()
 	if instance != "character.Character" {
 		t.Errorf("Not proper type, %v received", instance)
 	}
 }
 
+func TestErrorWhenWrongUlid(t *testing.T) {
+	_, err := NewCharacter("asd")
+	if err == nil {
+		t.Errorf("Should return error when wrong ULID")
+	}
+}
+
 func TestValues(t *testing.T) {
-	sut := dummyCharacter()
+	sut, _ := dummyCharacter()
 	if sut.Id() != ULID {
 		t.Errorf("Not matching ids")
 	}
 }
 
-func dummyCharacter() Character {
-	characterId, _ := NewCharacterId(ULID)
-	return Character{characterId}
+func dummyCharacter() (Character, error) {
+	return NewCharacter(ULID)
 }
-
-// func TestInvoke(t *testing.T) {
-// if (sut.Id() != string(characterId.value())) {
-// t.Errorf("Not matching ids")
-// }
-//   response := httptest.NewRecorder()
-//   request := httptest.NewRequest("GET", "/", nil)
-//   Ready(response, request)
-//   if response.Code != http.StatusOK {
-//     t.Errorf("Expected response code %d. Got %d\n", http.StatusNotFound, response.Code)
-//   }
-// }
