844546e2a2ba6d2906ffca9ddb1d3924df878366
Xan Bellón <xan.bellon@gmail.com>
2024-01-11 08:11:28 +0100

    feat: added basic initialization for Character

--- backend/src/domain/character/character.go
+++ backend/src/domain/character/character.go
@@ -1,12 +1,12 @@
 package character
 
-func NewCharacter (ulid string, name string, actualXp int, maxHp int) (Character, error) {
-}
+// func NewCharacter (ulid string, name string, actualXp int, maxHp int) (Character, error) {
+// }
 
 type Character struct {
   characterId CharacterId
 }
 
-func (c *Character) id() string {
+func (c *Character) Id() string {
 	return c.characterId.value()
 }
--- backend/src/domain/character/character_test.go
+++ backend/src/domain/character/character_test.go
@@ -1,14 +1,31 @@
 package character
 
 import (
+	"reflect"
 	"testing"
 )
 
-// func TestExists(t *testing.T) {
-//
-// }
+func TestInstanceOf(t *testing.T) {
+	characterId := FromEmpty()
+	sut := Character{characterId}
+	instance := reflect.TypeOf(sut).String()
+	if instance != "character.Character" {
+		t.Errorf("Not proper type, %v received", instance)
+	}
+}
+
+func TestValues(t *testing.T) {
+	characterId := FromEmpty()
+	sut := Character{characterId}
+	if sut.Id() != characterId.value() {
+		t.Errorf("Not matching ids")
+	}
+}
 
 // func TestInvoke(t *testing.T) {
+// if (sut.Id() != string(characterId.value())) {
+// t.Errorf("Not matching ids")
+// }
 //   response := httptest.NewRecorder()
 //   request := httptest.NewRequest("GET", "/", nil)
 //   Ready(response, request)
