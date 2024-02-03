e645e9b035dc0d2fc4c5e2fddc1e022ab26d5dc5
Xan Bellón <xan.bellon@gmail.com>
2024-02-03 11:21:40 +0100

    chore: remove old go backend
    Signed-off-by: Xan Bellón <xan.bellon@gmail.com>

--- backend/src/readyness/infrastructure/entrypoint/api/not_found_controller.go
+++
@@ -1,8 +0,0 @@
-package api
-
-import ("net/http")
-
-func NotFound(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusNotFound)
-}
