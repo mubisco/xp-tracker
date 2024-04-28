419b5cf4aea1f46f547d574ed6c28897f9ff50ac
Xan Bellón <xan.bellon@gmail.com>
2024-04-28 14:10:11 +0200

    feat: implemented interfaces

--- backend/src/Encounter/Infrastructure/Persistence/EventStoreEncounterRepository.php
+++ backend/src/Encounter/Infrastructure/Persistence/EventStoreEncounterRepository.php
@@ -5,12 +5,21 @@ declare(strict_types=1);
 namespace XpTracker\Encounter\Infrastructure\Persistence;
 
 use XpTracker\Encounter\Domain\AddEncounterWriteModel;
+use XpTracker\Encounter\Domain\BasicEncounter;
 use XpTracker\Encounter\Domain\Encounter;
 use XpTracker\Encounter\Domain\EncounterAlreadyExistsException;
+use XpTracker\Encounter\Domain\EncounterNotFoundException;
+use XpTracker\Encounter\Domain\EncounterRepository;
+use XpTracker\Encounter\Domain\UpdateEncounterWriteModel;
+use XpTracker\Shared\Domain\Event\EmptyEventsForCollectionException;
 use XpTracker\Shared\Domain\Event\EventCollection;
+use XpTracker\Shared\Domain\Identity\SharedUlid;
 use XpTracker\Shared\Infrastructure\Persistence\EventStore;
 
-final class EventStoreEncounterRepository implements AddEncounterWriteModel
+final class EventStoreEncounterRepository implements
+    AddEncounterWriteModel,
+    UpdateEncounterWriteModel,
+    EncounterRepository
 {
     public function __construct(private readonly EventStore $eventStore)
     {
@@ -26,5 +35,25 @@ final class EventStoreEncounterRepository implements AddEncounterWriteModel
         }
         $collection = EventCollection::fromValues($encounter->id(), $encounter->pullEvents());
         $this->eventStore->appendEvents($collection);
+    }
+
+    public function updateMonsters(Encounter $update): void
+    {
+        $results = $this->eventStore->getEventsForUlid($update->id());
+        if (empty($results)) {
+            throw new EncounterNotFoundException("No encounter found with {$update->id()} ulid for update");
+        }
+        $collection = EventCollection::fromValues($update->id(), $update->pullEvents());
+        $this->eventStore->appendEvents($collection);
+    }
+
+    public function byEncounterId(SharedUlid $encounterId): Encounter
+    {
+        try {
+            $eventCollection = $this->eventStore->eventCollection($encounterId);
+            return BasicEncounter::fromEvents($eventCollection);
+        } catch (EmptyEventsForCollectionException) {
+            throw new EncounterNotFoundException("No encounter found with {$encounterId->ulid()} ulid");
+        }
     }
 }
