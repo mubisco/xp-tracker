28357e7e7ba09db9bca1f1703f706774343d97a8
Xan Bellón <xan.bellon@gmail.com>
2024-05-02 18:33:21 +0200

    feat: add quick queryhandler for parties

---
+++ backend/src/Character/Application/Query/FindAllPartiesQuery.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace XpTracker\Character\Application\Query;
+
+final class FindAllPartiesQuery
+{
+}
---
+++ backend/src/Character/Application/Query/FindAllPartiesQueryHandler.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace XpTracker\Character\Application\Query;
+
+use Doctrine\DBAL\Connection;
+
+final class FindAllPartiesQueryHandler
+{
+    public function __construct(private readonly Connection $connection)
+    {
+    }
+    /**
+     * @return array<int,array<string,mixed>>
+     */
+    public function __invoke(FindAllPartiesQuery $query): array
+    {
+        $sql = "SELECT p.party_id AS partyUlid, p.name AS partyName, COUNT(pc.character_id) AS characterQuantity
+            FROM party p LEFT JOIN party_character pc ON pc.party_id = p.party_id
+            GROUP BY pc.party_id;";
+        return $this->connection->fetchAllAssociative($sql);
+    }
+}
