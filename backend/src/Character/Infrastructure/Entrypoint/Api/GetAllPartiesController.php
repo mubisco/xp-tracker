50a2fcd82272d1e4d9326d3150001b4808e067c3
Xan Bellón <xan.bellon@gmail.com>
2024-05-02 18:33:36 +0200

    feat: integrate query handler in GetAllPartiesController

--- backend/src/Character/Infrastructure/Entrypoint/Api/GetAllPartiesController.php
+++ backend/src/Character/Infrastructure/Entrypoint/Api/GetAllPartiesController.php
@@ -8,13 +8,20 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Attribute\Route;
+use XpTracker\Character\Application\Query\FindAllPartiesQuery;
+use XpTracker\Character\Application\Query\FindAllPartiesQueryHandler;
 
 #[AsController]
 #[Route("/api/party", name: "api_party_all", methods: ['GET'])]
 final class GetAllPartiesController
 {
+    public function __construct(private readonly FindAllPartiesQueryHandler $useCase)
+    {
+    }
     public function __invoke(): JsonResponse
     {
-        return new JsonResponse([], Response::HTTP_OK);
+        $query = new FindAllPartiesQuery();
+        $results = ($this->useCase)($query);
+        return new JsonResponse($results, Response::HTTP_OK);
     }
 }
