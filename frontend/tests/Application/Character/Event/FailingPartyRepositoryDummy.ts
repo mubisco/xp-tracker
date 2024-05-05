38b55e49e4bcb2b09c8db22dacf478a60bba7642
Xan Bellón <xan.bellon@gmail.com>
2024-05-05 15:33:44 +0200

    chore: remove tests for Character namespace

--- frontend/tests/Application/Character/Event/FailingPartyRepositoryDummy.ts
+++
@@ -1,9 +0,0 @@
-import { Party } from '@/Domain/Character/Party/Party'
-import { PartyRepository } from '@/Domain/Character/Party/PartyRepository'
-import { PartyRepositoryError } from '@/Domain/Character/Party/PartyRepositoryError'
-
-export class FailingPartyRepositoryDummy implements PartyRepository {
-  find (): Promise<Party> {
-    throw new PartyRepositoryError('Opsie')
-  }
-}
